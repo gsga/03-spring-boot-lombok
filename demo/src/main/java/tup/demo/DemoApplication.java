27c7ae081fa0e118351834667f9e50815fc0a57f
gsga <ggarciaa@gmail.com>
2022-05-03 15:45:57 -0300

    A

---
+++ demo/src/main/java/tup/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package tup.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import tup.demo.models.User;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hola(@RequestParam(value = "name", defaultValue = "José") String name) {
+		User user = new User();
+		user.setName(name);
+
+		return String.format("¡Hola %s!", user.getName());
+	}
+}
---
+++ demo/src/main/java/tup/demo/models/User.java
@@ -0,0 +1,10 @@
+package tup.demo.models;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+
+}
